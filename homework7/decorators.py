b2f8d74ab30c614c59836cc4c6792e7fc33b0c3f
Kaktysig <Kaktysig@MacBook-Air-Kaktys.local>
2017-11-02 19:32:43 +0300

    ready

--- homework7/decorators.py
+++ homework7/decorators.py
@@ -7,25 +7,16 @@ def cancel(func):
 
 
 def count_execution(func):
-    # def wrapper():
-    #     wrapper.count += 1
-    #     print("{0} была вызвана: {1}x".format(func.__name__, wrapper.count))
-    #
-    # wrapper.count = 0
-    # return wrapper
-    import time
-
-    def wrappers():
-        t = time.clock()
-        return func.__name__, time.clock() - t
-        yield wrappers
+    def gen_count():
+        count = 0
+        while True:
+            yield count
+            count += 1
 
     def wrapper():
-        wrapper.called += 1
-        return func()
+        gen = gen_count()
+        return next(gen)
 
-    wrapper.called = 0
-    wrapper.__name__ = func.__name__
     return wrapper
 
 
